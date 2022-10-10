b3a6c86153594ce79f2cbf4ee6e2ec6f86b581c0
zodress <108037881+zodress@users.noreply.github.com>
2022-10-10 16:01:58 +0000

    Update info-request.js

--- plugins/info-request.js
+++ plugins/info-request.js
@@ -5,7 +5,7 @@ let text = `${htki} *REQUEST* ${htka}
         Klik link di bawah ini
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Link', url: 'wa.me/6283839200413'}},
+    {index: 1, urlButton: {displayText: 'Link', url: 'wa.me/6285172402754'}},
 ]
 let tm = {
 text: text,
